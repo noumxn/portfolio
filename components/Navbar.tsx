f5861b88b585fe7676092d8352a378003fd77480
Nouman Syed <syednouman2114@gmail.com>
2024-01-03 17:53:40 -0500

    bugfix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,16 +35,7 @@ export default function Navbar() {
       <div className="justify-between md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <Link 
-              to="home"
-              className="container flex items-center space-x-2"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-                <h2 className="text-2xl font-bold">Nouman Syed</h2>
-            </Link>
+            <h2 className="container flex items-center space-x-2 text-2xl font-bold">Nouman Syed</h2>
             <div className="md:hidden">
               <button
                 className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
